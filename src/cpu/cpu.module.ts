6038223cb550c55d673e714b3ff607003e5f97c9
tirzasrwn <tirzasrwn@gmail.com>
2024-04-17 14:25:31 +0700

    feat: Connecting power module to cpu module.

--- src/cpu/cpu.module.ts
+++ src/cpu/cpu.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CpuService } from './cpu.service';
 import { CpuController } from './cpu.controller';
+import { PowerModule } from 'src/power/power.module';
 
 @Module({
   providers: [CpuService],
-  controllers: [CpuController]
+  controllers: [CpuController],
+  imports: [PowerModule]
 })
-export class CpuModule {}
+export class CpuModule { }
